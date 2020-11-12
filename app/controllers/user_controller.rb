9a02b27b76494adddbfb7997089a043228a2a523
Mateusz Moneta <mateusz.moneta@pwc.com>
2020-11-12 12:27:32 +0100

    Add user list action

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,16 @@
 class UserController < ApplicationController
+  def list
+    users = User
+      .left_outer_joins(:user_type)
+      .select("users.id, users.first_name, users.last_name, users.email, user_types.name")
+
+    if (params[:user_type])
+      return render json: users.where(:user_types => { name: params[:user_type] }).all
+    end
+
+    return render json: users.all
+  end
+
   def login
     @user = User
       .left_outer_joins(:user_type)
@@ -9,17 +21,17 @@ class UserController < ApplicationController
       return render json: { id: @user.id, user_type: @user.user_type.name }
     end
 
-    return render json: { messageKey: 'USER_HAS_NOT_EXIST' }
+    return render json: { messageKey: 'USER_HAS_NOT_EXIST' }, status: :unprocessable_entity
   end
 
   def register
     if (User.find_by({ email: params[:email], password: params[:password] }))
-      return render json: { messageKey: 'USER_ALREADY_EXIST' }
+      return render json: { messageKey: 'USER_ALREADY_EXIST' }, status: :unprocessable_entity
     end
 
     @user = User.new register_params
     if (@user.save)
-      return render json: { messageKey: 'USER_HAS_BEEN_CREATED' }
+      return render json: @user, status: :created
     end
   end
 
